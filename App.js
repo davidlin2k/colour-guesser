d90c8b763ad9e99bcf3fd7726ed64fa6848295f7
David Lin <davidzylin@outlook.com>
2022-03-01 21:43:40 -0500

    Source Code

---
+++ App.js
@@ -0,0 +1,159 @@
+import React, { useState, useReducer } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
+
+const RandomRGB = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  return [ red, green, blue ];
+};
+
+const GetColorHint = (guessedAmount, actualAmount) => {
+  if (guessedAmount != '') {
+    const difference = guessedAmount - actualAmount;
+    if (Math.abs(difference) <= 10) {
+      if (guessedAmount > actualAmount) {
+        return 'Close but too much';
+      }
+      else if (guessedAmount < actualAmount) {
+        return 'Close but less than the actual amount';
+      }
+      else {
+        return 'Correct!';
+      }
+    }
+    else {
+      return 'Not Quite';
+    }
+  }
+  else {
+    return 'Guess a number first';
+  }
+};
+
+var [ red, green, blue ] = RandomRGB();
+
+const createAlert = () =>
+  Alert.alert(
+    "Win!",
+    "You guessed the correct combination!",
+    [
+      {
+        text: "Continue",
+      },
+    ]
+);
+
+export default function App() {
+  const [ point, setPoint ] = useState(0);
+
+  const [ refresh, setRefresh ] = useState(false);
+
+  const reducer = (state, action) => {
+    switch (action.color) {
+        case 'red':
+          return {...state, guessed_red: action.amount};
+        case 'green':
+          return {...state, guessed_green: action.amount};
+        case 'blue':
+          return {...state, guessed_blue: action.amount};
+        default:
+            return state
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, { guessed_red:'', guessed_green:'', guessed_blue:'' });
+  const { guessed_red, guessed_green, guessed_blue } = state
+
+  const refreshColor = () => {
+    [red, green, blue] = RandomRGB();
+
+    dispatch({color: 'red', amount: ''});
+    dispatch({color: 'green', amount: ''});
+    dispatch({color: 'blue', amount: ''});
+
+    setRefresh(!refresh);
+  };
+
+  const CheckWin = (g_red, a_red, g_green, a_green, g_blue, a_blue) => {
+    if (g_red == a_red && g_blue == a_blue && g_green == a_green) {
+      setPoint(point + 1);
+      refreshColor();
+      createAlert();
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>Guess the Colour</Text>
+      <Text>Point: {point}</Text>
+      <View style={ {...styles.colorBox, backgroundColor: `rgb(${red}, ${green}, ${blue})` } }></View>
+      <Text>Red ({GetColorHint(guessed_red, red)})</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        onChangeText={ (newText) => {
+          dispatch({color: 'red', amount: newText});
+          CheckWin(newText, red, guessed_green, green, guessed_blue, blue);
+        }}
+        value={guessed_red}
+      />
+      <Text>Green ({GetColorHint(guessed_green, green)})</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        onChangeText={ (newText) => {
+          dispatch({color: 'green', amount: newText});
+          CheckWin(guessed_red, red, newText, green, guessed_blue, blue);
+        }}
+        value={guessed_green}
+      />
+      <Text>Blue ({GetColorHint(guessed_blue, blue)})</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        onChangeText={ (newText) => {
+          dispatch({color: 'blue', amount: newText});
+          CheckWin(guessed_red, red, guessed_green, green, newText, blue)
+        }}
+        value={guessed_blue}
+      />
+      <Text>{red}, {green}, {blue}</Text>
+      <Button
+        title='Update Color'
+        onPress={ () => {
+          refreshColor();
+        }}
+      />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fffff2'
+  },
+  input: {
+    margin: 15,
+    width: 100,
+    borderColor: 'black',
+    backgroundColor: 'white',
+    borderWidth: 1
+  },
+  colorBox: {
+    margin: 30,
+    height: 100,
+    width: 100,
+    borderColor: 'black',
+    borderWidth: 1
+  },
+});
